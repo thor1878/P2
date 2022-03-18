f8f5ddc340abe07852a4f4924cc6c077de6e168a
KORFITZ1DEV <cmor21@student.aau.dk>
2022-03-18 12:53:06 +0100

    server update

--- testserver/src/app.js
+++ testserver/src/app.js
@@ -2,19 +2,30 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+const PORT = 3000;
+
+app.post("/", (req, res) => {
+    console.log(req.body);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-
-// Simulate receiving source code from GitHub Action
-
-
-// Run existing tests
-
-
-// Generate 
 
 
 
