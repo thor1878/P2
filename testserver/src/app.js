924c37d53de3ce0037e01dc2ad7d113ecf73d8b7
thor1878 <thorbjoern9@live.dk>
2022-03-18 09:12:44 +0100

    add todos to testserver

--- testserver/src/app.js
+++ testserver/src/app.js
@@ -1,12 +1,20 @@
-const fs = require('fs');
 
-// Simulate receiving source code from GitHub Action
-
-
-// Run existing tests
+// GitHub Action triggered
 
 
-// Generate 
+// Make API call to GitHub with parameters received from GitHub Actions
+
+
+// Fetch all the files with '.js' extension and save them as strings
+
+
+// Search the strings to extract the functions defined with the function keyword
+
+
+// Update the ??? file
+
+
+// Generate test cases to all the functions
 
 
 
