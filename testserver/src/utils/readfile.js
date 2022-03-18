e490dcf5e9565887694e82a1491db036cbfeeffd
thor1878 <thorbjoern9@live.dk>
2022-03-18 09:59:37 +0100

    smal update

--- testserver/src/utils/readfile.js
+++ testserver/src/utils/readfile.js
@@ -1,4 +1,43 @@
 
+// GitHub Action triggered
+
+
+// Make API call to GitHub with parameters received from GitHub Actions
+
+
+// Fetch all the files with '.js' extension and save them as strings
+
+
+// Search the strings to extract the functions defined with the function keyword
+
+
+// Update the ??? file
+
+
+// Generate test cases to all the functions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // `
