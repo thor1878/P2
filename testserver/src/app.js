9b3b0959e88f0028e0097d0aca3072b6afe4c94a
KORFITZ1DEV <cmor21@student.aau.dk>
2022-03-18 12:16:46 +0100

    Express testserver setup

---
+++ testserver/src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+// Simulate receiving source code from GitHub Action
+
+
+// Run existing tests
+
+
+// Generate 
+
+
+
+
+
