29448a03235108d08fe1f2c23cef0bf2658290fc
Rasmus <rasmu@LAPTOP-08K6AG11>
2022-03-18 15:20:24 +0100

    Created submit form to test getting input to the server from the front-end

--- webapp/src/app.js
+++ webapp/src/app.js
@@ -6,11 +6,23 @@ const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', 'src/public/views');
+app.use(express.urlencoded({ extended: true }));
 app.use('/static', express.static('src/public/static'));
 
 app.get('/', (req, res) => {
     res.render('index', {functions: dummyData.functions});
 })
+
+app.get('/submit', (req, res) => {
+    res.render("submit");
+})
+
+app.post('/submit', (req, res) => {
+    console.log(req.body);
+    res.send("Done");
+})
+
+
 
 app.listen(PORT, () => {
     console.log(`Server running on Port ${PORT}`);
