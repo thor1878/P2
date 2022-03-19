f067b666c90f4686c4768b233b4ceeef3cfa39af
thor1878 <thorbjoern9@live.dk>
2022-03-19 18:48:32 +0100

    testing getFileData

--- testserver/src/utils/getData.js
+++ testserver/src/utils/getData.js
@@ -1,46 +1,56 @@
-//filter in file structure
+// Return an array containing only the objects with a path having the '.js' extension (not including '.test.js')
 function filterData(data) {
-    let filteredData = [];
-    for (let file of data.tree) {
-        if (file.path.slice(-3) == ".js") {
-            filteredData.push(file);
-        }
-    }
-    return filteredData;
+    // let filteredData = [];
+    // for (let file of data.tree) {
+    //     if (file.path.slice(-3) === '.js' && file.path.slice(-7) !== '.test.js') {
+    //         filteredData.push(file);
+    //     }
+    // }
+    // return filteredData;
+
+    return data.tree.filter(file => file.path.slice(-3) === '.js' && file.path.slice(-7) !== '.test.js');
 }
 
-async function getFileData(filtredData, url) {
+async function getFileData(filteredData, url) {
     let fileData = [];
     for (let file of filteredData) {
-        let functionString = {}
-        functionString.path = file.path
-        functionString.functions = []
 
         let response = await fetch(file.url);
         let data = await response.json();
 
-        let decodedFile = atob(data.content);
+        let fileString = data.content.toString('base64');
 
-        getFunctionStrings(decodedFile)
+        fileData.push({
+            path: file.path,
+            fileString: fileString
+        })
 
 
     }
+
+    return fileData;
 }
 
-function getFunctionStrings(decodedFile) {
-    let functionStrings = [];
-
-    decodedFile.
-
-
-    return functionarray
-}
-
-module.exports = filter;
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = filterData;
 
 
 
