82843bf6277b38cf3157e6dcb196e410e458f43f
Jojokp2000 <johannes@karstoft.eu>
2022-03-21 15:32:15 +0100

    extended test generation and structurefile

--- testserver/src/utils/generateTests.js
+++ testserver/src/utils/generateTests.js
@@ -1,22 +1,29 @@
-function generateTests(data){
-    generatedTests = [];
+const fs = require('fs');
+
+function generateTests(){
+    let data = fs.readFileSync("../../../structure-test.json");
+    let generatedTests = [];
+    let files = JSON.parse(data).files;
+
+    for (const file of files) {
+        for (const func of file.functions) {
+            for (const tc of func.testCases) {
+                let functionString = `test(${tc.description}, async () => {
+                    const result = await ${func.name}(${JSON.stringify([...tc.arguments])});
+                    expect(result).${tc.matcher}(${tc.expected});
+                })`
+                console.log(functionString);
+            }
+        }
+    }
 }
 
-// file path
-// function name
-// params
-// expected
-// matcher
-// description
-// status
+
+generateTests();
 
 
 
-`
-test($, async () => {
-    const result = await $($);
-    expect(result).$($);
-})
-`
+
+
 
 
