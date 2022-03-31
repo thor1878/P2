105a54e83f4eff25fcf435af799874f49e582f31
Frihauge <38877854+MathiasFrihauge@users.noreply.github.com>
2022-03-31 10:38:56 +0200

    sexy dropdown

--- webapp/src/public/static/js/choose_repo.js
+++ webapp/src/public/static/js/choose_repo.js
@@ -1,8 +1,14 @@
+
 let buttons = document.querySelectorAll('.card');
+
+
 let selected = localStorage.getItem("activeCard");
 
-for (const button of buttons) {
 
+
+for (const button of buttons) {
+   
+   
     if(button.id === selected)
     {
         button.classList.add('active');
@@ -12,14 +18,38 @@ for (const button of buttons) {
         
         for (const button of buttons) {
             button.classList.remove('active');
+           
+            //det er det samme som dropdown lol
+            button.nextElementSibling.classList.remove("show");
+                   
         }
+
         button.classList.add('active');
+        activate_dropdown(button);
+
         selected = button.id;
         localStorage.setItem("activeCard",selected);
     });
 
+
+
 }
 
+function activate_dropdown(btn){
 
 
+    let dropdown = btn.nextElementSibling;
 
+    if(btn.className == "card active")
+    {
+        dropdown.classList.add("show");
+        console.log("diller haha")
+    }
+    else
+    {
+        dropdown.classList.remove("show");
+    }
+    
+   
+
+}
