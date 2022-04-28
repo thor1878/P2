e55d93e027c5a4914bbc51ab3f0484294764f24d
Jonas Geertsen Lund <jgl21@student.aau.dk>
2022-04-28 17:25:29 +0200

    Removed console.log

--- testserver/src/utils/getData.js
+++ testserver/src/utils/getData.js
@@ -16,7 +16,6 @@ async function getRepoData(repository, branch, gh_token) {
 // Return an array containing only the objects with a path having the '.js' extension (not including '.test.js')
 // and exclude the .js files in the .github folder
 function filterRepoData(repoData) {
-    console.log(repoData);
     // Match anything not starting with '.github/'
     // Then match anything ending with '.js' if '.test' has not preceded it
     return repoData.tree.filter(file => file.path.match(/^(?!.github\/).*?(?<!\.test)(\.js)$/));
