e0489e5fe646b7eb83d1fda5469c0de7da1ae9a2
Frihauge <38877854+MathiasFrihauge@users.noreply.github.com>
2022-04-28 13:30:46 +0200

    step sis bumping on other steps

--- webapp/src/public/static/js/testanim.js
+++ webapp/src/public/static/js/testanim.js
@@ -4,7 +4,7 @@ function reveal() {
   for (var i = 0; i < reveals.length; i++) {
     let windowHeight = window.innerHeight;
     let elementTop = reveals[i].getBoundingClientRect().top;
-    let elementVisible = 150;
+    let elementVisible = 15;
 
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active");
