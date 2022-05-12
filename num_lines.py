7c560fe3cdf9d363b8fd975112b730361bbbdf2f
thor1878 <thorbjoern9@live.dk>
2022-05-12 11:07:33 +0200

    update line counter

--- num_lines.py
+++ num_lines.py
@@ -1,32 +1,45 @@
 import os
 import re
+import sys
 
-num_lines = 0
-num_files = 0
+sys.stdout = open('output.txt', 'w')
 
-print('\n\n')
+FOLDERS = ['.git', 'node_modules', 'coverage']
+FILES = ['package-lock.json', '.gitignore']
+
+total_lines = 0
+total_files = 0
+
+print('-' * 100)
+print('Lines'.rjust(5) + '  |  ' + 'File'.ljust(30) + '\t' + 'Path')
+print('-' * 100)
+
 for root, dirs, files in os.walk('.'):
     if len(root.split('/')) < 2:
         continue
-    if '.git' in root.split('/') or 'node_modules' in root.split('/') or 'package-lock' in root.split('/'):
-        # print(root.split('/'))
-        continue
+    for folder in FOLDERS:
+        if folder in root.split('/'):
+            break
+    else:
 
-    for file in files:
-        if file == 'package-lock.json': continue
-        num_files += 1
-        print(file)
-        path = f'{root}/{file}'
-        try:
-            with open(path, 'r') as f:
-                for line in f.readlines():
-                    num_lines += 1
-                
-                
-                
-        except:
-            # print(f'couldn\'t read {path}')
-            pass
+        for file in files:
+            if file in FILES: continue
+            
+            path = f'{root}/{file}'
+            try:
+                with open(path, 'r') as f:
+                    num_lines = 0
+                    for line in f.readlines():
+                        num_lines += 1
+                        total_lines += 1
+                total_files += 1
+                print(f'{str(num_lines).rjust(5)}  |  {file.ljust(30)}\t{root}')
+                print('-' * 100)
+            except:
+                # print(f'couldn\'t read {path}')
+                pass
 
-print(num_files)
-print(num_lines)
+print(f'Total lines: {total_lines}\tTotal files: {total_files}')
+print('-' * 100)
+
+sys.stdout.close()
